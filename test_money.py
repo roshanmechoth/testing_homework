e8680ae955e2d7f6247472a8f2de562f472a7fcb
Roshan <roshanmechoth@gmail.com>
2016-12-16 04:37:33 -0800

    changed test file of money

--- test_money.py
+++ test_money.py
@@ -1,11 +1,4 @@
-def change(money):
-    s=[]
-    for i in [1000,500,100,50,20,10,1]:
-        denom = money/i
-        if denom > 0:
-          a='{} * {} = {}'.format( i,denom,i*denom)
-          s.append(a)
-          money = money - (i * denom )
-    return s
-change(1500)
-
+import money
+def test_breakdown():
+    b=money.change(1500)
+    assert b==['1000 * 1 = 1000','500 * 1 = 500']
